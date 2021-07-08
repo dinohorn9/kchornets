5cad0f5f830d3a0b825e14ab548dc0415ef6498e
dinohorn9 <86866389+dinohorn9@users.noreply.github.com>
2021-07-08 10:04:11 -0500

    img to Image
    fix for v11 of nextjs

--- pages/index.js
+++ pages/index.js
@@ -40,97 +40,13 @@ export default function Home() {
               </p>
             </div>
             <div className="flex items-center">
-              <img
-                className="w-10 h-10 rounded-full mr-4"
+              <Image
                 src={"./images/jonathan.jpg"}
+                className="w-10 h-10 rounded-full mr-4"
                 height="40"
                 width="40"
                 alt="Avatar of Jonathan Reinink"
-              />
-              <div className="text-sm">
-                <p className="text-yellow-900 leading-none">Jonathan Reinink</p>
-                <p className="text-yellow-600">Aug 18</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="max-w-sm w-full lg:max-w-full lg:flex p-4">
-          <div
-            className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-            style={{ backgroundImage: "url('/images/card-left.jpg')" }}
-            title="Woman holding a mug"
-          ></div>
-          <div className="border-r border-b border-l border-yellow-400 lg:border-l-0 lg:border-t lg:border-yellow-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-            <div className="mb-8">
-              <p className="text-sm text-red-600 flex items-center">
-                <svg
-                  className="fill-current text-red-500 w-3 h-3 mr-2"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
-                </svg>
-                Members only
-              </p>
-              <div className="text-gray-900 font-bold text-xl mb-2">
-                11u AA Tryout Form
-              </div>
-              <p className="text-gray-700 text-base">
-                Please fill out the form for either July 10th or July 17th.
-              </p>
-            </div>
-            <div className="flex items-center">
-              <img
-                className="w-10 h-10 rounded-full mr-4"
-                src={"./images/jonathan.jpg"}
-                height="40"
-                width="40"
-                alt="Avatar of Jonathan Reinink"
-              />
-              <div className="text-sm">
-                <p className="text-yellow-900 leading-none">Jonathan Reinink</p>
-                <p className="text-yellow-600">Aug 18</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="max-w-sm w-full lg:max-w-full lg:flex p-4">
-          <div
-            className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-            style={{ backgroundImage: "url('/images/card-left.jpg')" }}
-            title="Woman holding a mug"
-          ></div>
-          <div className="border-r border-b border-l border-yellow-400 lg:border-l-0 lg:border-t lg:border-yellow-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-            <div className="mb-8">
-              <p className="text-sm text-red-600 flex items-center">
-                <svg
-                  className="fill-current text-red-500 w-3 h-3 mr-2"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
-                </svg>
-                Members only
-              </p>
-              <div className="text-gray-900 font-bold text-xl mb-2">
-                Can coffee make you a better developer?
-              </div>
-              <p className="text-gray-700 text-base">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Voluptatibus quia, nulla! Maiores et perferendis eaque,
-                exercitationem praesentium nihil.
-              </p>
-            </div>
-            <div className="flex items-center">
-              <img
-                className="w-10 h-10 rounded-full mr-4"
-                src={"./images/jonathan.jpg"}
-                height="40"
-                width="40"
-                alt="Avatar of Jonathan Reinink"
-              />
+              ></Image>
               <div className="text-sm">
                 <p className="text-yellow-900 leading-none">Jonathan Reinink</p>
                 <p className="text-yellow-600">Aug 18</p>
