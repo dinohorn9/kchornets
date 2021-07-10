cc673736e9e4ceca29f79d602f9491278c3ad9e4
dinohorn9 <86866389+dinohorn9@users.noreply.github.com>
2021-07-10 08:33:02 -0500

    Content Update
    Rained out

--- pages/index.js
+++ pages/index.js
@@ -32,6 +32,47 @@ export default function Home() {
                 Members only
               </p> */}
               <div className="text-gray-900 font-bold text-xl mb-2">
+                KC Hornet 11u AA BB Tryout Update
+              </div>
+              <p className="text-gray-700 text-base">
+                <b className="text-red-500">Today's Status:</b> We are rained out this afternoon. This first tryout has been rescheduled to Tuesday, July 13th @ 6-8pm.  Please contact me 
+                if you can make this tryout. The forecast for the 17th doesn't look good either.  Thanks!
+                <br />
+              </p>
+              <p>
+                <b>Contact Us:</b> dinohorn9@gmail.com
+              </p>
+              <p>
+                <b>Call/Text:</b> 913.306.1393
+              </p>
+            </div>
+            <div className="flex items-center">
+              <div className="text-sm">
+                <p className="text-yellow-900 leading-none">Dino Horn</p>
+                <p className="text-yellow-600">July 8th</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full p-4 ">
+          {/* <div
+            className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+            style={{ backgroundImage: "url('/images/card-left.jpg')" }}
+            title="Woman holding a mug"
+          ></div> */}
+          <div className="border-r border-yellow-400 border-l border-t border-b border-yellow-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+            <div className="mb-8">
+              {/* <p className="text-sm text-red-600 flex items-center">
+                <svg
+                  className="fill-current text-red-500 w-3 h-3 mr-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
+                </svg>
+                Members only
+              </p> */}
+              <div className="text-gray-900 font-bold text-xl mb-2">
                 KC Hornet 11u AA Baseball Fall Tryout Info
               </div>
               <p className="text-gray-700 text-base">
